bc1b6836330249404ae3093c751bc4d42045c62a
erniealice <16433795+erniealice@users.noreply.github.com>
2026-03-16 15:33:13 +0800

    20260316: Remove job, job_activity, and job_template views migrated to fayna package

--- views/job/embed.go
+++
@@ -1,6 +0,0 @@
-package job
-
-import "embed"
-
-//go:embed templates/*.html
-var TemplatesFS embed.FS
--- views/job_template/embed.go
+++
@@ -1,6 +0,0 @@
-package job_template
-
-import "embed"
-
-//go:embed templates/*.html
-var TemplatesFS embed.FS
